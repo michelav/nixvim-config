18c9df78d66a3943fc218adf64bf815cdcd066e1
michelav <michel.vasconcelos@gmail.com>
2025-09-03 15:24:47 -0300

    chore(cmp): Add Tab and Super-Tab selection

--- config/base/core.nix
+++ config/base/core.nix
@@ -53,41 +53,41 @@
     cmp = {
       enable = true;
       autoEnableSources = true;
+      cmdline = {
+        "/" = {
+          mapping = {
+            __raw = "cmp.mapping.preset.cmdline()";
+          };
+          sources = [
+            {
+              name = "buffer";
+            }
+            { name = "nvim_lsp_document_symbol"; }
+            { name = "cmdline"; }
+          ];
+        };
+        ":" = {
+          mapping = {
+            __raw = "cmp.mapping.preset.cmdline()";
+          };
+          sources = [
+            {
+              name = "path";
+            }
+            {
+              name = "cmdline";
+              option = {
+                ignore_cmds = [
+                  "Man"
+                  "!"
+                ];
+              };
+            }
+          ];
+        };
+      };
       settings = {
         completion.autocomplete = [ "require('cmp.types').cmp.TriggerEvent.TextChanged" ];
-        cmdline = {
-          "/" = {
-            mapping = {
-              __raw = "cmp.mapping.preset.cmdline()";
-            };
-            sources = [
-              {
-                name = "buffer";
-              }
-              { name = "nvim_lsp_document_symbol"; }
-              { name = "cmdline"; }
-            ];
-          };
-          ":" = {
-            mapping = {
-              __raw = "cmp.mapping.preset.cmdline()";
-            };
-            sources = [
-              {
-                name = "path";
-              }
-              {
-                name = "cmdline";
-                option = {
-                  ignore_cmds = [
-                    "Man"
-                    "!"
-                  ];
-                };
-              }
-            ];
-          };
-        };
         window = {
           completion = {
             border = "rounded";
@@ -120,6 +120,37 @@
               ['<C-Space>'] = cmp.mapping.complete(),
               ['<C-e>'] = cmp.mapping.abort(),
               ['<CR>'] = cmp.mapping.confirm({ select = true }),
+              ['<CR>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    if luasnip.expandable() then
+                        luasnip.expand()
+                    else
+                        cmp.confirm({
+                            select = true,
+                        })
+                    end
+                else
+                    fallback()
+                end
+              end),
+              ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.locally_jumpable(1) then
+                  luasnip.jump(1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+              ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.locally_jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
             })
           '';
       };
